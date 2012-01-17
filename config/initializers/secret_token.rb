4bbbb1aea482c96f98c4008d63a813163dc22163
Cameron Chang <cameron.chang@orionvm.com.au>
2012-01-17 13:44:31 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboard::Application.config.secret_token = '97ca46458a09190ac9884a285c6b7cd8b4767449daddafd97776d91773ed114c827195079bda03eeb6885f7551a6df3ec38bdc6f576d61c17445d64804e72922'
